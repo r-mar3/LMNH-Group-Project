81ffb265f9253190d60e591d4fb0a4f78f233dc7
SamiL52 <samilachqar@gmail.com>
2025-11-12 10:59:35 +0000

    Added functionality to upload all provided table data

--- load.py
+++ load.py
@@ -1,7 +1,22 @@
+"""Loads all local table data and uploads them to the RDS"""
 import pyodbc
 from os import environ
 from dotenv import load_dotenv
 import pandas as pd
+
+DATA_FILEPATH = './data/clean_data/'
+CSV_FILETYPE = '.csv'
+TABLE_NAMES = [
+    'license',
+    'species',
+    'image',
+    'botanist',
+    'plant',
+    'country',
+    'reading',
+    'origin',
+    'city'
+]
 
 
 def get_db_connection() -> pyodbc.Connection:
@@ -13,15 +28,15 @@ def get_db_connection() -> pyodbc.Connection:
         f'DATABASE={environ['DB_NAME']};'
         f'UID={environ['DB_USER']};'
         f'PWD={environ['DB_PASSWORD']};'
-        f'TrustServerCertificate=yes;'
+        'TrustServerCertificate=yes;'
     )
 
     return conn
 
 
-def load_csv(filepath: str) -> pd.DataFrame:
-    """Returns a dataframe of the csv at a given filepath"""
-    return pd.read_csv(filepath)
+def load_csv(table_name: str) -> pd.DataFrame:
+    """Returns a dataframe of the csv for a given table name"""
+    return pd.read_csv(f'{DATA_FILEPATH}{table_name}{CSV_FILETYPE}')
 
 
 def upload_table_data(conn: pyodbc.Connection, table_name: str, df: pd.DataFrame) -> None:
@@ -35,16 +50,14 @@ def upload_table_data(conn: pyodbc.Connection, table_name: str, df: pd.DataFrame
         VALUES
             ({column_placeholders});
     """
-    print(sql_query)
 
     cur = conn.cursor()
     cur.executemany(sql_query, [tuple(row) for row in df.to_numpy()])
-    print("ROWCOUNT:", cur.rowcount)
     cur.close()
 
 
 if __name__ == '__main__':
-    with get_db_connection() as conn:
-        table_name = 'country'
-        country_df = load_csv(f'{table_name}.csv')
-        upload_table_data(conn, table_name, country_df)
+    with get_db_connection() as connection:
+        for table_name in TABLE_NAMES:
+            table_df = load_csv(table_name)
+            upload_table_data(connection, table_name, table_df)
