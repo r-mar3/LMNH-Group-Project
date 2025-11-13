ec32317bf0b43e16a62a695be01b1f142b0fd925
DevMjee <dev.mjee2@gmail.com>
2025-11-13 15:52:01 +0000

    trying to add foreign keys

--- load.py
+++ load.py
@@ -9,63 +9,58 @@ import numpy as np
 from pprint import pprint
 
 DATA_FILEPATH = './data/clean_data.csv'
-# BASE_TABLES = [
-#     'country',
-#     'botanist',
-#     'license'
-# ]
 
-# FOREIGN_TABLES = [
-#     'city',
-#     'origin',
-#     'image',
-#     'species',
-#     'plant',
-#     'reading'  # keep adding rows to this, allow duplicates!
-# ]
-
-TABLES = {
-    'country': {
+TABLES = [
+    {
+        'table_name': 'country',
         'columns': ['country_name'],
         'unique_column': 'country_name'
     },
-    'botanist': {
+    {
+        'table_name': 'botanist',
         'columns': ['botanist_name', 'botanist_email', 'botanist_phone'],
         'unique_column': 'botanist_email'
     },
-    'license': {
+    {
+        'table_name': 'license',
         'columns': ['license_name', 'license_number', 'license_url'],
         'unique_column': 'license_number'
     }
-}
+]
 
-FOREIGN_TABLES = {
-    'city': {
+FOREIGN_TABLES = [
+    {
+        'table_name': 'city',
         'columns': ['city_name', 'country_id'],
         'unique_column': 'city_name'
     },
-    'origin': {
+    {
+        'table_name': 'origin',
         'columns': ['origin_longitude', 'origin_latitude', 'city_id'],
         'unique_column': ['origin_longitude', 'origin_latitude']
     },
-    'image': {
+    {
+        'table_name': 'image',
         'columns': ['image_original_url', 'image_regular_url', 'image_medium_url', 'image_small_url', 'image_thumbnail_url', 'license_id'],
         'unique_column': 'image_original_url'
     },
-    'species': {
+    {
+        'table_name': 'species',
         'columns': ['species_name', 'species_scientific_name', 'image_id'],
         'unique_column': 'species_name'
     },
-    'plant': {
+    {
+        'table_name': 'plant',
         'columns': ['plant_id', 'species_id', 'origin_id'],
         'unique_column': 'plant_id'
     },
-    'reading': {
+    {
+        'table_name': 'reading',
         'columns': ['reading_last_watered', 'reading_time_taken', 'reading_soil_moisture', 'reading_temperature', 'reading_error', 'botanist_id', 'plant_id'],
         'unique_column': ''
     }
 
-}
+]
 
 
 def get_db_connection() -> pyodbc.Connection:
@@ -83,32 +78,90 @@ def get_db_connection() -> pyodbc.Connection:
     return conn
 
 
-def upload_table_data_with_foreign_key(conn: pyodbc.Connection) -> None:
-    sql_query = """
-        INSERT INTO
-            city (city_name, country_id)
-        SELECT
-            city_name, country_id
-        WHERE
-    """
-
-
-def upload_table_data(conn: pyodbc.Connection, table_name: str, df: pd.DataFrame, unique_col: str) -> None:
-    """Uploads the data in the given dataframe to the matching table in the database"""
-    columns = list(df.columns)
+def upload_table_data_with_foreign_key(conn: pyodbc.Connection, table_dict: dict, df: pd.DataFrame) -> None:
+    # all column names
+    columns = table_dict['columns']
     column_names = ', '.join(columns)
-    column_placeholders = ', '.join('?' for _ in columns)
+    # column names which only appear in this table
+    local = [col for col in columns if not col.endswith('_id')]
+    # local_names = ', '.join(local)
+    # foreign table names which are mentioned
+    all_foreign_keys = [col for col in columns if col.endswith('_id')]
+    all_foreign_names = [col.replace('_id', '') for col in all_foreign_keys]
+
+    column_placeholders = ', '.join('?' for _ in local)
+    table_name = table_dict['table_name']
+    unique_col = table_dict['unique_column']
+
     sql_query = f"""
         INSERT INTO
             {table_name} ({column_names})
         SELECT
-            {column_placeholders}
+            {column_placeholders},
+            """
+
+    all_foreign_unique_columns = local
+    for foreign_key in all_foreign_keys:
+        foreign_name = foreign_key.replace('_id', '')
+
+        for table in FOREIGN_TABLES:
+            if table['table_name'] == foreign_name:
+                foreign_table = table
+                all_foreign_unique_columns.append(table['unique_column'])
+
+        for table in TABLES:
+            if table['table_name'] == foreign_name:
+                foreign_table = table
+                all_foreign_unique_columns.append(table['unique_column'])
+
+        sql_query += f"""(SELECT
+                {foreign_key}
+            FROM
+                {foreign_table['table_name']}
+            WHERE
+                {foreign_table['unique_column']} = ?)
+
+            """
+
+    df = df[all_foreign_unique_columns]
+
+    sql_query += f"""
         WHERE NOT EXISTS
-            (SELECT 1 FROM {table_name} WHERE {unique_col} = ?)
+            (SELECT 1 FROM {table_name} WHERE {unique_col}= ?)
         ;
     """
-    print(sql_query)
+
+    sql_params = []
+    for index, row in enumerate(df.to_numpy()):
+        something = np.append(row, df[unique_col][index])
+        sql_params.append(tuple(np.append(something, ',')))
+
+    print(sql_params)
     input()
+
+    cur = conn.cursor()
+    cur.executemany(sql_query, sql_params)
+    cur.close()
+
+
+def upload_table_data(conn: pyodbc.Connection, table_dict: dict, df: pd.DataFrame) -> None:
+    """Uploads the data in the given dataframe to the matching table in the database"""
+    columns = table_dict['columns']
+    column_names = ', '.join(columns)
+    column_placeholders = ', '.join('?' for _ in columns)
+    table_name = table_dict['table_name']
+    unique_col = table_dict['unique_column']
+
+    sql_query = f"""
+        INSERT INTO
+            {table_name}({column_names})
+        SELECT
+            {column_placeholders}
+        WHERE NOT EXISTS
+            (SELECT 1 FROM {table_name} WHERE {unique_col}= ?)
+        ;
+    """
+
     df = df.dropna().reset_index()
 
     # params for executemany must be a tuple, and must include the
@@ -125,26 +178,12 @@ def upload_table_data(conn: pyodbc.Connection, table_name: str, df: pd.DataFrame
 
 if __name__ == '__main__':
     clean_table = pd.read_csv(DATA_FILEPATH)
-    # tables = {}
-
-    # refactor to a function
-    # columns = clean_table.columns.tolist()
-
-    # for tablename in BASE_TABLES:
-    #     tables[tablename] = {
-    #         'columns': [col for col in columns if tablename + '_' in col],
-    #         'unique': [col for col in columns if '_name' in col]
-    #     }
-
-    # for tablename in FOREIGN_TABLES:  # add a list of foreign col names for add 'foreign'+_id
-    #     tables[tablename] = [col for col in columns if tablename + '_' in col]
-
     with get_db_connection() as connection:
-        # add base tables with auto generated ids
-        # for table_name, columns in tables.items():
-        #     upload_table_data(connection, table_name,
-        #                       clean_table[columns], unique_col=)
+
         for table in TABLES:
-            print(table)
             upload_table_data(
-                connection, table, clean_table[TABLES[table]['columns']], TABLES[table]['unique_column'])
+                conn=connection, table_dict=table, df=clean_table[table['columns']])
+
+        for table in FOREIGN_TABLES:
+            upload_table_data_with_foreign_key(
+                conn=connection, table_dict=table, df=clean_table)
