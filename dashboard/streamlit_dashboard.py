5033528525bda9bd32bf49c0bddb339fd0f8a998
Ronn Marakkalsherry <ronnm03@gmail.com>
2025-11-14 12:49:45 +0000

    partially created dashboard

---
+++ dashboard/streamlit_dashboard.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+
+def dashboard_design(data: pd.DataFrame):
+
+    st.title("Dashboard")
+
+    alert_data = alerts_over_time_data(data)
+    temperature_data = temperature_over_time_data(data)
+    moisture_data = moisture_over_time_data(data)
+    botanist_alert_data = most_alerted_botanist_data(data)
+    with st.container(horizontal=True):
+        with st.container(horizontal=False):
+            st.altair_chart(temperature_over_time_chart(temperature_data))
+            st.altair_chart(alerts_over_time_chart(alert_data))
+            st.altair_chart(most_alerted_botanist_chart(botanist_alert_data))
+        with st.container(horizontal=False):
+            st.altair_chart(moisture_over_time_chart(moisture_data))
+            st.altair_chart(count_of_alerts(alert_data))
+
+
+def alerts_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
+    "creates a dataframe with just the plant, alert and time data"
+
+    alerts = data[['reading_time_taken', 'reading_alert', 'species_name']]
+    alerts['reading_time_taken'] = pd.to_datetime(data['reading_time_taken'])
+    alerts['reading_date'] = alerts['reading_time_taken'].dt.date
+    alerts['reading_time'] = alerts['reading_time_taken'].dt.time
+    alerts = alerts.drop('reading_time_taken', axis=1)
+
+    return alerts
+
+
+def alerts_over_time_chart(data: pd.DataFrame):
+    "creates a line graph of alerts over time"
+
+    chart = alt.Chart(data).mark_circle().encode(
+        alt.X('reading_time'),
+        alt.Y('reading_alert'),
+        alt.Color('species_name', legend=None)
+    ).properties(height=300)
+    return chart
+
+
+def count_of_alerts(data: pd.DataFrame):
+
+    data['reading_alert'] = data['reading_alert'].astype(int)
+    alerts = data.groupby(by='species_name')[
+        'reading_alert'].sum().reset_index(name='count of alerts')
+
+    chart = alt.Chart(alerts).mark_bar().encode(
+        alt.Color('species_name'),
+        alt.X('species_name'),
+        alt.Y('count of alerts')
+    )
+
+    return chart
+
+
+def temperature_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
+
+    temperature_data = data[['reading_temperature',
+                             'reading_time_taken', 'species_name']]
+    # temperature_data = temperature_data[temperature_data['reading_temperature']]
+    temperature_data['reading_time_taken'] = pd.to_datetime(
+        data['reading_time_taken'])
+    temperature_data['reading_date'] = temperature_data['reading_time_taken'].dt.date
+    temperature_data['reading_time'] = temperature_data['reading_time_taken'].dt.time
+
+    return temperature_data
+
+
+def temperature_over_time_chart(data: pd.DataFrame):
+
+    chart = alt.Chart(data).mark_circle().encode(
+        alt.X('reading_time'),
+        alt.Y('reading_temperature'),
+        alt.Color('species_name', legend=None)
+    )
+
+    return chart
+
+
+def moisture_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
+
+    moisture_data = data[['reading_soil_moisture',
+                          'reading_time_taken', 'species_name']]
+    moisture_data['reading_time_taken'] = pd.to_datetime(
+        data['reading_time_taken'])
+    moisture_data['reading_date'] = moisture_data['reading_time_taken'].dt.date
+    moisture_data['reading_time'] = moisture_data['reading_time_taken'].dt.time
+
+    return moisture_data
+
+
+def moisture_over_time_chart(data: pd.DataFrame):
+
+    chart = alt.Chart(data).mark_circle().encode(
+        alt.X('reading_time'),
+        alt.Y('reading_soil_moisture'),
+        alt.Color('species_name', legend=None
+                  )
+    )
+
+    return chart
+
+
+def most_alerted_botanist_data(data: pd.DataFrame) -> pd.DataFrame:
+
+    botanist_data = data[['botanist_name', 'reading_alert']]
+    botanist_data['reading_alert'] = botanist_data['reading_alert'].astype(int)
+
+    botanist_data = botanist_data.groupby(by='botanist_name')[
+        'reading_alert'].sum().reset_index(name='alert count')
+
+    return botanist_data
+
+
+def most_alerted_botanist_chart(data: pd.DataFrame):
+
+    chart = alt.Chart(data).mark_bar().encode(
+        alt.X('botanist_name'),
+        alt.Y('alert count'),
+        alt.Color('botanist_name', legend=None)
+    )
+    return chart
+
+
+if __name__ == '__main__':
+    input_data = pd.read_csv('./clean_data.csv')
+
+    dashboard_design(input_data)
