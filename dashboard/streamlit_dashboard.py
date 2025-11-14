99983c715cf397b4591fcd7b817c69d378193959
SamiL52 <samilachqar@gmail.com>
2025-11-14 15:01:08 +0000

    Reformatted dashboard

--- dashboard/streamlit_dashboard.py
+++ dashboard/streamlit_dashboard.py
@@ -6,20 +6,27 @@ from extract_dashboard import get_all_data
 
 def dashboard_design(data: pd.DataFrame):
 
-    st.title("Dashboard")
-
+    st.set_page_config(page_title="LMNH Plant Dashboard", layout='wide')
+    st.title('LMNH Plant Dashboard')
     alert_data = alerts_over_time_data(data)
     temperature_data = temperature_over_time_data(data)
     moisture_data = moisture_over_time_data(data)
     botanist_alert_data = most_alerted_botanist_data(data)
-    with st.container(horizontal=True):
-        with st.container(horizontal=False):
-            st.altair_chart(temperature_over_time_chart(temperature_data))
-            st.altair_chart(alerts_over_time_chart(alert_data))
-            st.altair_chart(most_alerted_botanist_chart(botanist_alert_data))
-        with st.container(horizontal=False):
-            st.altair_chart(moisture_over_time_chart(moisture_data))
-            st.altair_chart(count_of_alerts(alert_data))
+
+    print(temperature_data)
+
+    st.subheader('Plant Temperature')
+    st.altair_chart(temperature_over_time_chart(temperature_data))
+
+    st.subheader('Plant Alerts')
+    st.altair_chart(alerts_over_time_chart(alert_data))
+    st.altair_chart(count_of_alerts(alert_data))
+
+    st.subheader('Botanists')
+    st.altair_chart(most_alerted_botanist_chart(botanist_alert_data))
+
+    st.subheader('Soil Moisture')
+    st.altair_chart(moisture_over_time_chart(moisture_data))
 
 
 def alerts_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
@@ -34,27 +41,29 @@ def alerts_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
     return alerts
 
 
-def alerts_over_time_chart(data: pd.DataFrame):
+def alerts_over_time_chart(data: pd.DataFrame) -> alt.Chart:
     "creates a line graph of alerts over time"
 
-    chart = alt.Chart(data).mark_circle().encode(
-        alt.X('reading_time'),
-        alt.Y('reading_alert'),
-        alt.Color('species_name', legend=None)
+    chart = alt.Chart(data).mark_line().encode(
+        x=alt.X(field='reading_time', title='Reading Time', type='temporal'),
+        y=alt.Y(field='reading_alert', title='Number of Alerts'),
+        color=alt.Color('species_name', legend=None)
     ).properties(height=300)
     return chart
 
 
-def count_of_alerts(data: pd.DataFrame):
+def count_of_alerts(data: pd.DataFrame) -> alt.Chart:
 
     data['reading_alert'] = data['reading_alert'].astype(int)
     alerts = data.groupby(by='species_name')[
         'reading_alert'].sum().reset_index(name='count of alerts')
 
     chart = alt.Chart(alerts).mark_bar().encode(
-        alt.Color('species_name'),
-        alt.X('species_name'),
-        alt.Y('count of alerts')
+        color=alt.Color(field='species_name'),
+        x=alt.X(field='species_name', type='nominal',
+                title='Species Name', sort='-y'),
+        y=alt.Y(field='count of alerts', type='quantitative',
+                title='Number of Alerts')
     )
 
     return chart
@@ -73,10 +82,10 @@ def temperature_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
     return temperature_data
 
 
-def temperature_over_time_chart(data: pd.DataFrame):
+def temperature_over_time_chart(data: pd.DataFrame) -> alt.Chart:
 
-    chart = alt.Chart(data).mark_circle().encode(
-        alt.X('reading_time'),
+    chart = alt.Chart(data).mark_line().encode(
+        alt.X('reading_time_taken'),
         alt.Y('reading_temperature'),
         alt.Color('species_name', legend=None)
     )
@@ -96,7 +105,7 @@ def moisture_over_time_data(data: pd.DataFrame) -> pd.DataFrame:
     return moisture_data
 
 
-def moisture_over_time_chart(data: pd.DataFrame):
+def moisture_over_time_chart(data: pd.DataFrame) -> alt.Chart:
 
     chart = alt.Chart(data).mark_circle().encode(
         alt.X('reading_time'),
@@ -119,12 +128,14 @@ def most_alerted_botanist_data(data: pd.DataFrame) -> pd.DataFrame:
     return botanist_data
 
 
-def most_alerted_botanist_chart(data: pd.DataFrame):
+def most_alerted_botanist_chart(data: pd.DataFrame) -> alt.Chart:
 
     chart = alt.Chart(data).mark_bar().encode(
-        alt.X('botanist_name'),
-        alt.Y('alert count'),
-        alt.Color('botanist_name', legend=None)
+        x=alt.X(field='botanist_name', type='nominal',
+                sort='-y', title='Botanist Name'),
+        y=alt.Y(field='alert count', title='Number of Alerts',
+                type='quantitative'),
+        color=alt.Color('botanist_name', legend=None)
     )
     return chart
 
